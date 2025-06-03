304181eca8abd18b4e706f892347d6f765080a7b
Ameen <mohamedameen.21.2001@outlook.com>
2025-06-04 00:38:37 +0530

    installed shadcn and added a buttom component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </>
   );
 }
